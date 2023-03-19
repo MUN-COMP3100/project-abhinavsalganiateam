6dcc98f1070dbfc4eed59531cddd25b7022e3947
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-03-19 20:16:12 -0230

    add movie shows API test

---
+++ test/movie-show-timings.js
@@ -0,0 +1,16 @@
+// Test function
+async function testDisplayMovieShowTimings() {
+    // Call the displayMovieShowTimings function
+    await displayMovieShowTimings();
+    
+    // Verify that the showtimes are displayed on the webpage
+    const showtimesElement = document.getElementById('movie-showtimes');
+    if (showtimesElement.innerHTML === '') {
+      console.error('Error: Showtimes not displayed on the webpage.');
+    } else {
+      console.log('Test passed: Showtimes displayed on the webpage.');
+    }
+  }
+  
+  // Example usage
+  testDisplayMovieShowTimings();
