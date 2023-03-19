18c4982e04924c8e8574365f78aa204393869094
Abhinav Salgania <abhinavsalgania@gmail.com>
2023-03-19 14:40:29 -0230

    update searchController

--- controller/searchController.js
+++ controller/searchController.js
@@ -10,3 +10,63 @@ export const searchMovies = async (req, res) => {
   }
 };
 
+/**
+ * Endpoint to get the details of a single movie
+ * @param {*} req - HTTP request object
+ * @param {*} res - HTTP response object
+ */
+export const getMovieDetails = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const movie = await MovieBusiness.getMovieDetails(id);
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+/**
+ * Endpoint to add a new movie
+ * @param {*} req - HTTP request object
+ * @param {*} res - HTTP response object
+ */
+export const addMovie = async (req, res) => {
+  try {
+    const { title, description, releaseDate, runtime, genres, cast } = req.body;
+    const movie = await MovieBusiness.addMovie(title, description, releaseDate, runtime, genres, cast);
+    res.status(201).json(movie);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+/**
+ * Endpoint to update the details of a movie
+ * @param {*} req - HTTP request object
+ * @param {*} res - HTTP response object
+ */
+export const updateMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, description, releaseDate, runtime, genres, cast } = req.body;
+    const movie = await MovieBusiness.updateMovie(id, title, description, releaseDate, runtime, genres, cast);
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+/**
+ * Endpoint to delete a movie
+ * @param {*} req - HTTP request object
+ * @param {*} res - HTTP response object
+ */
+export const deleteMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await MovieBusiness.deleteMovie(id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
