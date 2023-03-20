24571f8666e504ff701656f798eec5ed1c63cbb0
rashodk <rmanamperiko@mun.ca>
2023-03-19 21:39:58 -0230

    update readme.md

--- test/API-tests.js
+++ test/API-tests.js
@@ -1,18 +1,19 @@
+import { getCity, getLocation, getMovieShowTimings } from ";
 async function testMovieShowTimings() {
   // Call the getCity function to get the city details
-  const cityData = await getCity('New York');
-  
+  const cityData = await getCity("New York");
+
   // Call the getLocation function to get the latitude and longitude of the city
   const locationData = await getLocation(cityData);
-  
+
   // Call the getMovieShowTimings function to get the movie timings for the given location
-  const movieData = await getMovieShowTimings(locationData.lat, locationData.lng, 'Avengers');
+  const movieData = await getMovieShowTimings(locationData.lat, locationData.lng, "Avengers");
 
   // Verify that the movie data is returned properly
   if (!movieData || !movieData.length) {
-    console.error('Error: Failed to retrieve movie timings for the given location and movie name.');
+    console.error("Error: Failed to retrieve movie timings for the given location and movie name.");
   } else {
-    console.log('Test passed: Movie timings retrieved successfully.');
+    console.log("Test passed: Movie timings retrieved successfully.");
   }
 }
 
